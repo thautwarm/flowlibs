c77f6b438c370fcd22c09c7196d7d0bed28cc7f4
thautwarm <twshere@outlook.com>
2017-08-25 23:53:30 +0800

    none

--- test.py
+++ test.py
@@ -6,23 +6,17 @@ Created on Fri Aug 25 07:40:40 2017
 @author: misakawa
 """
 
-#from flowlibs.rule.rule_url import img_downloader, headers
-#from flowlibs.rule.rule_str import Rule, re
-#
-#url = "http://www.apic.in/"
-#
-#main = re.compile('<img[\w\W]+?>')
-#rule = Rule(main)
-#selector = img_downloader(rule)
-#res  = selector.getfrom(url)
-#for i in range(1):
-#    r =  next(res)
+from flowlibs.rule.rule_url import img_downloader, headers
+from flowlibs.rule.rule_str import Rule, re
 
+url = "http://www.apic.in/"
 
-
-
-
-
+main = re.compile('<img[\w\W]+?>')
+rule = Rule(main)
+selector = img_downloader(rule)
+res  = selector.getfrom(url)
+for i in range(1):
+    r =  next(res)
 
 
 
